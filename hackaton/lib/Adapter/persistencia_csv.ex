5000acad08e8cf6eb04c3a8ed740f3b6cc486511
Santiago <santiago.nietob@uqvirtual.edu.co>
2025-11-02 22:02:36 -0500

    modificacion persistencia

--- hackaton/lib/Adapter/persistencia_csv.ex
+++ hackaton/lib/Adapter/persistencia_csv.ex
@@ -1,17 +1,24 @@
-defmodule Hackaton.Adapter.PersistenciaCSV do
-  @moduledoc "Gestión de archivos CSV para persistencia de datos"
+defmodule Persistencia.CSV do
+  @moduledoc "Lectura/escritura CSV sencilla "
 
-  def leer(ruta) do
-    case File.read(ruta) do
-      {:ok, contenido} ->
-        contenido
-        |> String.split("\n", trim: true)
-        |> Enum.map(&String.split(&1, ","))
-      _ -> []
-    end
+  def leer_rutas!(ruta) do
+    ruta
+    |> File.read!()
+    |> String.split("\n", trim: true)
+    |> Enum.drop_while(&(&1 == ""))
+    |> Enum.map(&String.split(&1, ","))
   end
 
-  def guardar(ruta, fila) do
-    File.write!(ruta, Enum.join(fila, ",") <> "\n", [:append])
+  def escribir_rutas!(ruta, filas, encabezado \\ nil) do
+    contenido =
+      (if encabezado, do: [encabezado | filas], else: filas)
+      |> Enum.map(&Enum.join(&1, ","))
+      |> Enum.join("\n")
+
+    File.write!(ruta, contenido <> "\n")
+  end
+
+  def agregar_fila!(ruta, fila_lista) do
+    File.write!(ruta, Enum.join(fila_lista, ",") <> "\n", [:append])
   end
 end
